71f89a4c982920550a7f224ce2e24339b44071b1
Jeremy Bouchard <mukki1996@gmail.com>
2019-08-22 21:11:45 -0400

    Modified how City are loaded

--- src/city.py
+++ src/city.py
@@ -1,29 +1,39 @@
 import json
 from urllib.request import urlretrieve
-
 import xml.etree.ElementTree as ElementTree
 
 
 class City:
-    cities = {}
+    def __init__(self, use_json=True, url='http://dd.weather.gc.ca/citypage_weather/xml/siteList.xml'):
+        self.cities = []
 
-    def __init__(self, use_json=True, city_url='http://dd.weather.gc.ca/citypage_weather/xml/siteList.xml'):
-        if use_json is True:
+        if not self.cities:
+            self.get_cities(url, use_json)
+
+    def get_cities(self, url, use_json):
+        if use_json:
             with open('data/cities.json') as json_file:
                 self.cities = json.load(json_file)
-
         else:
-            urlretrieve(city_url, 'data/city.xml')
+            urlretrieve(url, 'data/city.xml')
 
             city_tree = ElementTree.ElementTree()
             city_tree.parse('data/city.xml')
             cities = city_tree.findall("site")
 
             for city in cities:
-                city_code = city.attrib["code"]
+                self.cities.append(
+                    {
+                        'id': city.attrib["code"],
+                        'english_name': city.findtext('nameEn'),
+                        'french_name': city.findtext('nameFr'),
+                        'province': city.findtext("provinceCode"),
+                    }
+                )
 
-                self.cities[city_code] = {
-                    'english_name': city.findtext('nameEn'),
-                    'french_name': city.findtext('nameFr'),
-                    'province': city.findtext("provinceCode"),
-                }
+    def find_city(self, city_name):
+        for city in self.cities:
+            if city_name == city['english_name'] or city_name == city['french_name']:
+                return city
+
+        return None
--- tests/test_city.py
+++ tests/test_city.py
@@ -1,21 +1,40 @@
-from urllib.error import URLError
-
 from src.city import City
 
+from xml.etree.ElementTree import ParseError
+
 from unittest import TestCase
-from unittest.mock import Mock
 
 
 class TestCity(TestCase):
-
-    def test_can_create_city_list_using_json(self):
-        City()
+    def setUp(self):
+        self.cities = City()
 
     def test_can_create_city_list_using_web_downloaded_xml(self):
         City(use_json=False)
 
     def test_city_with_json_are_same_as_city_with_xml(self):
-        cities_with_json = City()
+        cities_with_json = self.cities
         cities_with_xml = City(use_json=False)
 
-        self.assertEqual(cities_with_json.cities, cities_with_xml.cities)
+        self.assertListEqual(cities_with_json.cities, cities_with_xml.cities)
+
+    def test_can_get_correct_city_with_name(self):
+        dolbeau_mistassini_dict = {
+            'id': 's0000270',
+            'english_name': 'Dolbeau-Mistassini',
+            'french_name': 'Dolbeau-Mistassini',
+            'province': 'QC',
+        }
+
+        dolbeau_mistassini = self.cities.find_city('Dolbeau-Mistassini')
+
+        self.assertEqual(dolbeau_mistassini, dolbeau_mistassini_dict)
+
+    def test_cannot_get_City_that_doesnt_exist(self):
+        incorrect_city = self.cities.find_city('ABCDEFG')
+
+        self.assertIsNone(incorrect_city)
+
+    def test_bad_city_url_raises_exception(self):
+        with self.assertRaises(ParseError):
+            City(use_json=False, url='http://www.google.com')
